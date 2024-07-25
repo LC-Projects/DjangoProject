0ba68c5da7f735546d2e3394807a380600202e30
TeyKz <81241850+TeyKz@users.noreply.github.com>
2024-07-25 16:59:02 +0200

    Merge jonathan + test for password test

--- chatbot/accounts/tests.py
+++ chatbot/accounts/tests.py
@@ -50,6 +50,33 @@ class ProfileViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'accounts/profile.html')
 
+class SetPasswordViewTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        # Create a user for testing the set password view
+        cls.user = get_user_model().objects.create_user(username='testuser', password='12345')
+
+    def test_view_url_exists_at_desired_location(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get(reverse('auth:set_password'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_uses_correct_template(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.get(reverse('auth:set_password'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/change_password.html')
+
+    def test_password_change(self):
+        self.client.login(username='testuser', password='12345')
+        response = self.client.post(reverse('auth:set_password'), {
+            'old_password': '12345',
+            'new_password1': 'newpassword123',
+            'new_password2': 'newpassword123'
+        })
+        self.assertRedirects(response, reverse('auth:login'))
+        self.user.refresh_from_db()
+        self.assertTrue(self.user.check_password('newpassword123'))
 
 class UserModelTest(TestCase):
     def setUp(self):
