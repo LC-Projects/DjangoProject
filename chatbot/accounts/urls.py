1d7384c1fd0a780ef6c86b4eac3490327c242a2b
TeyKz <81241850+TeyKz@users.noreply.github.com>
2024-07-25 16:50:37 +0200

    Adding set password page + changing settings page

--- chatbot/accounts/urls.py
+++ chatbot/accounts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
-from .views import UserEditView, UserProfileEditView
+from .views import UserEditView, UserProfileEditView, ChangePasswordView
 
 app_name = 'accounts'
 
@@ -17,4 +17,5 @@ urlpatterns = [
     path('edit_profile/', UserProfileEditView.as_view(), name='edit_profile'),
     path('edit_mood/', views.edit_mood, name='edit_mood'),
     path('profile/<str:username>/', views.profile_view, name='profile'),
+    path('password/', ChangePasswordView.as_view(), name='set_password'),
 ]
