d7a00cc26debabb25008de4f45a411cf80cb78f6
LuckyMarty <luckymarty68@gmail.com>
2024-07-26 14:20:11 +0200

    🚩 TEST OK; Notifications

--- chatbot/notification/urls.py
+++ chatbot/notification/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("list/", views.FeedbackFilterView.as_view(), name="notification_list"),
     path("create/", views.NotificationView.as_view(), name="create"),
+    path("mark_as_read/<int:pk>/", views.mark_as_read, name="mark_as_read"),
 ]
